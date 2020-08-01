d8a9d933a98b6c5957b592406fbe9f25e7f924a0
mechAneko12 <naokih1998@yahoo.co.jp>
2020-08-01 17:46:34 +0900

    Aug 1, 2020 at 17:38

--- vr_project/settings.py
+++ vr_project/settings.py
@@ -157,7 +157,7 @@ if not DEBUG:
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
