e4ddf56e6cd13c738c69d8dc58dfea7d451aa649
mechAneko12 <naokih1998@yahoo.co.jp>
2020-08-07 21:19:30 +0900

    Aug 7, 2020 at 19:59

--- manager/models.py
+++ manager/models.py
@@ -8,5 +8,9 @@ class Wavelet(models.Model):
     class_label = models.IntegerField()
 
 class Weight(models.Model):
+    inpt_size = models.IntegerField(null=True)
+    hidden_size = models.IntegerField(null=True)
+    output_size = models.IntegerField(null=True)
+    layer_num = models.IntegerField(null=True)
     affine1 = ArrayField(ArrayField(models.FloatField(), size=240), size=300)
     affine2 = ArrayField(ArrayField(models.FloatField(), size=300), size=6)
