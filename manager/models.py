8b5207dcb5f56fac7b0a2529c5400bac0300d0de
mechAneko12 <naokih1998@yahoo.co.jp>
2020-08-07 20:18:52 +0900

    Aug 7, 2020 at 19:59

--- manager/models.py
+++ manager/models.py
@@ -7,6 +7,6 @@ class Wavelet(models.Model):
     wavelet = ArrayField(models.FloatField(), size=240)
     class_label = models.IntegerField()
 
-class Weights(models.Model):
+class Weight(models.Model):
     affine1 = ArrayField(ArrayField(models.FloatField(), size=1), size=1)
     affine2 = ArrayField(ArrayField(models.FloatField(), size=300), size=6)
